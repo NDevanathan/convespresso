02591471b9b1acf21050374ff683bdc3d46a0188
Nikhil Devanathan <nikhil.devanathan@gmail.com>
2023-05-21 13:32:54 -0700

    streamlined basic control

--- basic_control.py
+++ basic_control.py
@@ -3,25 +3,18 @@ from espresso import *
 
 PRE_INF_DUR = 8
 
-def temp_control(target, current, elapsed):
-    if elapsed >= PRE_INF_DUR:
-        target += 2
+def pre_infuse():
+    set_pump_level(1/6)
 
+def brew():
+    set_pump_level(3/6)
+
+def temp_control(target, current):
     alpha = 1/20
-    if current >= target:
-        heat_off()
-    else:
-        control = alpha * (target - current)
-        if control <= 2/6:
-            control = 2/6
-
-        set_heat_level(control)
+    control = alpha * (target - current)
+    if 0 <= control and control <= 2/6: control = 2/6
         
-def pressure_control(target, current, elapsed):
-    if elapsed < PRE_INF_DUR:
-        set_pump_level(1/6)
-    else:
-        set_pump_level(3/6)
+    set_heat_level(control)
 
 def test():
     boot_screen()
@@ -37,30 +30,30 @@ def test():
         cur_temp = poll_temp()
         cur_pres = poll_pressure()
         
-        if not SWT_MODE.value():
-            temp_targ = 125
-        else:
-            temp_targ = 92
+        temp_targ = 125 if not SWT_MODE.value() else 92
 
-        if not SWT_BREW.value():
+        if (not SWT_BREW.value()) and SWT_MODE.value():
             if not timing:
                 timing = True
                 start = time.ticks_ms()
+                
+            if timing:
+                seconds = time.ticks_diff(time.ticks_ms(), start) / 1000
+                open_valve()
+                
+                if seconds <= PRE_INF_DUR:
+                    pre_infuse()
+                else:
+                    brew()
+
         else:
             timing = False
-
-        if timing:
-            seconds = time.ticks_diff(time.ticks_ms(), start) / 1000
-            temp_control(temp_targ, cur_temp, seconds)
-        else:
-            temp_control(temp_targ, cur_temp, 0)
-
-        if not SWT_BREW.value():
-            open_valve()
-            pressure_control(pres_targ, cur_pres, seconds)
-        else:
             pump_off()
             close_valve()
+
+        set_temp = temp_targ
+        if timing and seconds <= PRE_INF_DUR: set_temp += 2
+        temp_control(set_temp, cur_temp)
 
         update_display(cur_temp, cur_pres, seconds, temp_targ, pres_targ, sec_targ)
         time.sleep(0.1)
